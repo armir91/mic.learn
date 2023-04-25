42da284da2b4ed33a571978493b586865310afb4
Armir <armir.keta@gmail.com>
2023-04-25 22:24:15 +0200

    Added TestProjects

---
+++ TestProject/Program.cs
@@ -0,0 +1,38 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+
+
+Console.WriteLine($"The total number of rolls equals to: {total}");
+
+if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+        Console.WriteLine($"Your new total is {total}");
+    } 
+    else
+    {
+        Console.WriteLine("Yeahhh  you rolled doubles! You get extra 2 points for that!!!");
+        total += 2;
+        Console.WriteLine($"Your new total is {total}");
+    }
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("Your total score is higher than 14! \nCongrats you are a winner");
+}
+else
+{
+    Console.WriteLine("Sorry, you didn't get a total higher than 14! \nSoooo, You looseeee bro!");
+}
---
+++ TestProject3/Program.cs
@@ -0,0 +1,37 @@
+﻿//exer 1
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+
+// var itemNumber = 0;
+// foreach (var item in fraudulentOrderIDs)
+// {
+//     Console.WriteLine($"The item number: {itemNumber} in the array is: {item}");
+//     itemNumber++;
+// }
+
+//extern 2
+// int[] inventory = { 200, 450, 700, 175, 250 };
+
+// int sum = 0;
+// foreach (int items in inventory)
+// {
+//     sum += items;
+// }
+
+// Console.WriteLine($"The sum of all array elements is: {sum}");
+
+//challenge
+string[] fraudulentOrderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+var counter = 1;
+foreach (var item in fraudulentOrderIDs)
+{
+    if (item.StartsWith('B'))
+    {
+        Console.WriteLine($"The number {counter} ID that  needs further investigation is: {item}");
+        counter++;
+    }
+}
