1bc939ae24d6683b39a71daa5bec020529f2487f
Armir <armir.keta@gmail.com>
2023-10-06 11:16:19 +0200

    aDDED NEW TESTPROJECT 5

---
+++ TestProject5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+var str = "a";
+var str1 = "a ";
+
+Console.WriteLine(str.Trim() == str1.Trim().ToUpper());
